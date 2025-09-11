b2d44477054fd27e4d53ce013de694005004b6f5
Juan Escobar <jescobar435@unab.edu.co>
2025-09-11 10:55:53 -0500

    Rutas básico

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Ruta productos
+Route::get('/products', function () {
+    return "Lista de productos";
+});
+
+Route::get('/products/create', function ()  {
+    return "Formulario de creación de productos";
+});
+
+Route::get('/products/{id}/{category?}', function ($name, $category = null)  {
+
+    if ($category === null) {
+        return "Detalle del producto: " . $name;
+    }
+
+    return "Detalle del producto: " . $name . "; de la categoría: " . $category;
+});
