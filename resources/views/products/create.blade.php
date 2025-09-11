fdb212d6ee8587971eb46267c79f6c8d03654e06
Juan Escobar <jescobar435@unab.edu.co>
2025-09-11 12:42:32 -0500

    Layouts básicos

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
+@extends('layouts.app')
+@section('content')
+
     <h1>Form for creating a new product</h1>
 
     <form action="" method="post">
@@ -32,5 +28,4 @@
         <label for="brand">Brand:</label>
         <input type="text" name="brand">
     </form>
-</body>
-</html>
+@endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>View of created products</h1>
-</body>
-</html>
+@endsection
+    
