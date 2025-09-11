0aa7f33b6f0a1679cfcdc92c3b3fc9ad8d527622
Juan Escobar <jescobar435@unab.edu.co>
2025-09-11 11:38:01 -0500

    Grupo de rutas básico

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,15 @@ Route::get('/', function () {
 });
 
 
-//Ruta productos
-Route::get('/products', [ProductController::class, 'index']);
+//Grupo de rutas de productos, con la anotación prefix se agrega el prefijo products a las rutas, que hace innecesario agregar el prefijo products a cada ruta, con controller
+//se agrega la anotación controller para que se pueda usar el controlador ProductController en las rutas.
+Route::prefix('products')->controller(ProductController::class)->group(function(){
+    
+    Route::get('/', 'index');
+    Route::get('/create', 'create');
+    Route::get('/{id}/{category?}', 'show');
+});
 
-Route::get('/products/create', [ProductController::class,'create']);
 
-Route::get('/products/{id}/{category?}', [ProductController::class, 'show']);
+
+
