488aa9412548e39ef61bb61456fb6721ded222af
Juan Escobar <jescobar435@unab.edu.co>
2025-10-02 12:17:38 -0500

    Factory Products failed

---
+++ app/Models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = "product";
+    protected $fillable = ["name", "price", "description", 'category_id', 'brand_id'];
+    public $timestamps = true;
+}
